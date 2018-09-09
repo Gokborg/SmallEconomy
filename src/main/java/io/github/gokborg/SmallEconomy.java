b4c1dc8a30b03cea5a2120ee0fba18d3d514871a
Ecconia <github-commit@ecconia.com>
2018-09-09 22:43:12 +0200

    Updating command registration to new JavaLoader version

--- src/main/java/io/github/gokborg/SmallEconomy.java
+++ src/main/java/io/github/gokborg/SmallEconomy.java
@@ -32,9 +32,9 @@ public class SmallEconomy extends JavaLoaderBukkitProject
 	public BukkitCommand[] getCommands()
 	{
 		return new BukkitCommand[] {
-			new BukkitCommand("pay", "Transfer money to other accounts.", "Usage: /pay [from] <to> <amount>.", "jl.economy.pay", (String[]) null, new PayCommand(bank), null),
-			new BukkitCommand("acc", "Account mangement commands", "Usage: /acc <create , bal, list, share, unshare>", "jl.economy.account", (String[]) null, new AccountCommand(bank), null),
-			new BukkitCommand("accmgm", "Admin commands for money", "Usage: /accmgm <give>", "jl.economy.accmgm", (String[]) null, new AdminCommand(bank), null)
+			new BukkitCommand("pay").setPermission("jl.economy.pay").setUsageMessage("Usage: /pay [from] <to> <amount>").setDescription("Transfer money to other accounts.").setExecutor(new PayCommand(bank)),
+			new BukkitCommand("acc").setPermission("jl.economy.account").setUsageMessage("Usage: /acc <create , bal, list, share, unshare>").setDescription("Account mangement commands").setExecutor(new AccountCommand(bank)),
+			new BukkitCommand("accmgm").setPermission("jl.economy.accmgm").setUsageMessage("Usage: /accmgm <give>").setDescription("Admin commands for money").setExecutor(new AdminCommand(bank))
 		};
 	}
 }
